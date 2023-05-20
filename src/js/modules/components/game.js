039fce9416e5b44f9d80fb93143177df29623dc7
MacJ13 <jacdim02@gmail.com>
2023-05-20 12:52:57 +0200

    Create new functions
    Create runGame, playGame, playComputerGame, switchPlayers functions
    Call OnclickonClickPlayBtn, onClickGameBoardPlayer2 function
    Modify createPlayer function and Game factory variables

--- src/js/modules/components/game.js
+++ src/js/modules/components/game.js
@@ -2,86 +2,43 @@ import UI from "./ui/ui";
 import Player from "./player";
 import GameUI from "./ui/gameUI";
 
-const Queue = function () {
-  let elements = {};
-  let head = 0;
-  let tail = 0;
-
-  function clearQueue() {
-    elements = {};
-    head = 0;
-    tail = 0;
-  }
-
-  function enqueue(element) {
-    elements[tail] = element;
-    tail++;
-  }
-
-  function dequeue() {
-    const item = elements[head];
-    delete elements[head];
-    head++;
-    return item;
-  }
-
-  function peek() {
-    return elements[head];
-  }
-
-  function countTypeShip() {
-    let count = 0;
-    for (const i in elements) {
-      const currentShip = elements[i];
-      if (peek().getLength() !== currentShip.getLength()) break;
-      count++;
-    }
-    return count;
-  }
-
-  function length() {
-    return tail - head;
-  }
-
-  function isEmpty() {
-    return length() == 0;
-  }
-
-  return {
-    enqueue,
-    dequeue,
-    peek,
-    length,
-    isEmpty,
-    countTypeShip,
-    clearQueue,
-  };
-};
+import { queue } from "./helpers";
 
 const Game = function () {
   const ui = UI();
   let gameUI = null;
   const players = [];
+
   let currentPlayer = null;
-  const queue = Queue();
+  let enemyPlayer = null;
+
+  let timeout = null;
+  // const queue = Queue();
 
   const createPLayers = function () {
-    // Create players
+    // get players info from inputs
     const playersUI = ui.getPlayerNames();
+    // console.log(playersUI);
 
-    for (let pl in playersUI) {
-      const name = playersUI[pl];
-      players.push(Player(name));
-    }
+    //  create players objects and add to players array
+    for (let { playerId, playerName } of playersUI)
+      players.push(Player(playerId, playerName));
 
-    currentPlayer = players[0];
-    for (let ship of players[0].getShips()) {
-      queue.enqueue(ship);
-    }
+    // set current Player
+    // currentPlayer = players[0];
 
+    // add player 1's ships to queue
+    queue.addItems(players[0].getShips());
+
+    // create GameInterface object
     gameUI = GameUI();
 
+    // call function to create gameBoards for players;
     createGameBoard();
+
+    // ????????????????????????????
+    // set players?
+    switchPlayers();
   };
 
   const setShipOnBoard = function (gameBoardObj) {
@@ -92,27 +49,82 @@ const Game = function () {
     if (!shipOnBoard) return;
 
     gameUI.renderShipOnGameBoard(gameBoardObj, queue.peek());
-
     queue.dequeue();
-
     gameUI.renderShipPick(queue.peek(), queue.countTypeShip());
   };
 
   const setRandomShipsOnBoard = function () {
     queue.clearQueue();
-    const randomGameBoard = players[0].getRandomShipsPosition();
-    gameUI.renderRandomGameBoard(randomGameBoard);
+    players[0].getRandomShipsPosition();
+    const gameBoard = players[0].getPlayerBoard();
+    gameUI.renderRandomGameBoard(gameBoard);
   };
 
   const clearGameBoard = function () {
-    queue.clearQueue();
     players[0].clearGameBoard();
-    for (let ship of players[0].getShips()) {
-      queue.enqueue(ship);
+    queue.addItems(players[0].getShips());
+
+    gameUI.clearGameBoard(players[0].getPlayerBoard());
+    gameUI.renderShipPick(queue.peek(), queue.countTypeShip());
+  };
+
+  const switchPlayers = function () {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    enemyPlayer = enemyPlayer === players[1] ? players[0] : players[1];
+  };
+
+  const playGameComputer = function () {
+    if (currentPlayer === players[0]) return;
+
+    timeout = setTimeout(() => {
+      console.log("Computer: ", { timeout });
+      const randomNumber = Math.floor(Math.random() * 100);
+      const pos = randomNumber < 10 ? "0" + randomNumber : "" + randomNumber;
+      timeout = null;
+      playGame(pos);
+    }, 500);
+  };
+
+  const playGame = function (pos) {
+    // console.log("player: ", { timeout });
+
+    if (timeout) {
+      return;
+    }
+    console.log("player: ", { timeout });
+    // pass pos variable to get gameboard cell of player2
+    const enemyCellBoard = currentPlayer.getPlayerBoardCell(enemyPlayer, pos);
+
+    // check if enemyCellBoard is falsy
+    if (!enemyCellBoard) {
+      if (currentPlayer === players[1]) playGameComputer();
+      return;
     }
 
-    gameUI.clearGameBoard(players[0]);
-    gameUI.renderShipPick(queue.peek(), queue.countTypeShip());
+    gameUI.renderTargetGameBoardCell(
+      pos,
+      enemyCellBoard.shipCell,
+      enemyPlayer.getGameId()
+    );
+
+    // console.log({ shipCell: enemyCellBoard.shipCell });
+
+    if (!enemyCellBoard.shipCell) {
+      switchPlayers();
+    }
+    playGameComputer();
+  };
+
+  // method set player1 ship board and call event listener for
+  // game board of player 2
+  const runGame = function () {
+    // set random ships for player 2
+    players[1].getRandomShipsPosition();
+
+    // players[1].saveAllPositionBoard(players[1].getPlayerBoard());
+    // call method with player 2 gameboard event listeners
+    gameUI.onClickGameBoardPlayer2(playGame);
+    console.log("run The Game");
   };
 
   const createGameBoard = function () {
@@ -123,9 +135,10 @@ const Game = function () {
     gameUI.renderShipPick(ship, countShipTypes);
 
     gameUI.onClickShipPick();
-    gameUI.onDropShipPick(setShipOnBoard, players[0].getGameBoard);
+    gameUI.onDropShipPick(setShipOnBoard, players[0].getPlayerBoard);
     gameUI.onClickResetBtn(clearGameBoard);
     gameUI.onClickRandomBtn(setRandomShipsOnBoard);
+    gameUI.onClickPlayBtn(runGame);
   };
 
   const init = function () {
