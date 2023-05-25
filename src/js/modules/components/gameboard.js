c593d7305d3ba2efe8460dbfe876a73f28ccd941
MacJ13 <jacdim02@gmail.com>
2023-05-25 13:06:46 +0200

    Update GameBoard factory function
    create "clearPotentialPosition", "getHitAdjacentPositions",
    "checkNextPotentialShipPosition", "createPotentialPosition"
    function
    
    create "potentialComputerShipPositions", "nextPositions",
    "potentialDirections" variables

--- src/js/modules/components/gameboard.js
+++ src/js/modules/components/gameboard.js
@@ -4,6 +4,18 @@ const Gameboard = function () {
   let board = [];
   const maxSize = 10;
 
+  // For Computer player to choose potential positions when
+  // ship is hit
+  let potentialComputerShipPositions = [];
+  let nextPosition = "";
+  let potentialDirections = [
+    [-1, 0], //  up next position
+    [0, 1], // right next position
+    [1, 0], // bottom next position
+    [0, -1], // left next position
+  ];
+  //////////////////////////////////////////////////////
+
   const clearGameBoard = function () {
     board.length = 0;
   };
@@ -160,7 +172,130 @@ const Gameboard = function () {
     }
   };
 
+  // add every possible direction to check computer positions array
+  const createPotentialPosition = function (posA, posB) {
+    // add possible direction to potential computer position array
+    for (let i = 0; i < potentialDirections.length; i++) {
+      const [x, y] = potentialDirections[i];
+      const posX = +posA + x;
+      const posY = +posB + y;
+      // check if potential direction exist
+      if (board[posX]?.[posY]) {
+        if (!board[posX][posY].marked) {
+          //  direction defines as index's number of potentionalPositions array
+          potentialComputerShipPositions.push({
+            position: [posX, posY],
+            direction: i,
+          });
+        }
+      }
+    }
+
+    // for (let potential of potentialComputerShipPositions) {
+    //   console.log(potential["position"], { direction: potential["direction"] });
+    // }
+  };
+
+  const checkNextPotentialShipPosition = function () {
+    function isEven(direction) {
+      return direction % 2 === 0;
+    }
+
+    function isOdd(direction) {
+      return direction % 2 === 1;
+    }
+
+    //1. first we get random number for index of array potentialComputerShipPositions
+    const randomNumber = Math.floor(
+      Math.random() * potentialComputerShipPositions.length
+    );
+
+    // eventually if element is not in array we return exist nextPostion
+    if (!potentialComputerShipPositions[randomNumber]) {
+      clearPotentialPosition();
+      return nextPosition;
+    }
+
+    // 2. next we get position and direction from element of potentialComputerShipPositions
+    const { position, direction } =
+      potentialComputerShipPositions[randomNumber];
+
+    //3. next we assign two positions two variables
+    const x = position[0];
+    const y = position[1];
+
+    // 4. we assing string with position one and position two
+    // and we extract ship cell, marked, res property from board elements with positions:
+    nextPosition = `${x}${y}`;
+    const { shipCell, marked, res } = board[x][y];
+
+    // 5a. condition if there is not ship cell we removing element from  potentialComputerShipPositions
+    if (!shipCell) {
+      potentialComputerShipPositions.splice(randomNumber, 1);
+    }
+    // //  5b. otherwise is is there and marked is false we remove and
+    else if (shipCell && !marked) {
+      // 5c. remove element from potentialComputerShipPositions array
+      potentialComputerShipPositions.splice(randomNumber, 1);
+
+      const correctDirection = direction % 2 === 0;
+
+      // filter elements only  with correct direction depending on ship postion on board
+      potentialComputerShipPositions = potentialComputerShipPositions.filter(
+        (potentionalPosition) => {
+          const { direction } = potentionalPosition;
+
+          const result = correctDirection
+            ? isEven(direction)
+            : isOdd(direction);
+          return result;
+          // correct direction is
+          // if (correctDirection) {
+          //   return isEven(direction);
+
+          // } else {
+          //   return isOdd(direction);
+          // }
+        }
+      );
+      // 5c. we set to check next position in the same direction like our element
+      const [dirX, dirY] = potentialDirections[direction];
+
+      const nextX = dirX + x;
+      const nextY = dirY + y;
+
+      // check if there is element for next position
+      if (board[nextX]?.[nextY]) {
+        // check if element on board is not marked and has ship object
+        if (!board[nextX][nextY].marked && board[nextX][nextY].shipCell) {
+          // we add next possible position to potential computer positions array
+          potentialComputerShipPositions.push({
+            position: [nextX, nextY],
+            direction,
+          });
+        }
+      }
+    }
+    // return position to check on board
+    return nextPosition;
+  };
+
+  // function to get possible next position for computer player
+  const getHitAdjacentPositions = function (posA, posB) {
+    if (potentialComputerShipPositions.length === 0)
+      createPotentialPosition(posA, posB);
+
+    return checkNextPotentialShipPosition();
+  };
+
+  // clear potential computer positions moves from array
+  const clearPotentialPosition = function () {
+    potentialComputerShipPositions.length = 0;
+  };
+
   return {
+    clearPotentialPosition,
+    getHitAdjacentPositions,
     setReservedCellBoard,
     placeShips,
     receiveAttack,
